0f93053e03c292d4c7d640526ae7de03fa2dc3ba
scasadykahler <scasadykahler3584@student.pps.net>
2024-06-06 12:00:38 -0700

    day 2
    damage enemy

---
+++ Assets/AIChase.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIChase : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+
+        transform.position = Vector2.MoveTowards();
+    }
+}
